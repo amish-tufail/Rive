4abf6b95e76311820a82dbe276524b461786adf8
Amish <amish.tufail@vyro.ai>
2024-03-22 14:38:56 +0500

    Initial Commit

---
+++ Rive/RiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiveApp.swift
+//  Rive
+//
+//  Created by mac on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct RiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
