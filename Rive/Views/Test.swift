02eb76888f1990a9a2efc06ce82c0467405a5523
Amish <amish.tufail2002@gmail.com>
2024-03-24 15:22:00 +0500

    -t

---
+++ Rive/Views/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  Rive
+//
+//  Created by Amish on 24/03/2024.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
