bb28e202d2b2b8dcc99b25e033aba27646da65ca
Amish <amish.tufail@vyro.ai>
2024-03-24 12:39:06 +0500

    - Onboarding Screen basic complete

---
+++ Rive/OnboardingView.swift
@@ -0,0 +1,56 @@
+//
+//  OnboardingView.swift
+//  Rive
+//
+//  Created by mac on 24/03/2024.
+//
+
+import SwiftUI
+import RiveRuntime
+
+struct OnboardingView: View {
+    let button =  RiveViewModel(fileName: "button")
+    var body: some View {
+        ZStack {
+            background
+            startButton
+        }
+    }
+}
+
+extension OnboardingView {
+    var background: some View {
+        RiveViewModel(fileName: "shapes").view()
+            .ignoresSafeArea()
+            .blur(radius: 30.0)
+            .background(
+                Image("Spline")
+                    .blur(radius: 50.0)
+                    .offset(x: 200.0, y: 100.0)
+            )
+    }
+    
+    var startButton: some View {
+        button.view()
+            .frame(width: 236.0, height: 64.0)
+            .overlay {
+                Label("Start the course", systemImage: "arrow.forward")
+                    .offset(x: 4.0, y: 4.0)
+                    .font(.headline)
+            }
+            .background(
+                Color.black
+                    .clipShape(.rect(cornerRadius: 10.0))
+                    .blur(radius: 10.0)
+                    .opacity(0.3)
+                    .offset(y: 10.0)
+            )
+            .onTapGesture {
+                button.play(animationName: "active")
+            }
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
