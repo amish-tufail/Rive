601ce7f4ada9cce1ed557f3e51050e9aeae7032c
Amish <amish.tufail2002@gmail.com>
2024-03-25 15:32:14 +0500

    - Side Menu Complete

--- Rive/Views/ContentView.swift
+++ Rive/Views/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
     let button = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
     var body: some View {
         ZStack {
+            Color.background2.ignoresSafeArea()
+            SideMenu()
+                .opacity(isOpen ? 1.0 : 0.0)
+                .offset(x: isOpen ? 0.0 : -300.0)
+                .rotation3DEffect(
+                    .degrees(isOpen ? 0.0 : 30), axis: (x: 0.0, y: 1.0, z: 0.0)
+                )
             Group {
                 switch selectedTab {
                 case .chat:
@@ -34,17 +41,30 @@ struct ContentView: View {
             .safeAreaInset(edge: .top) {
                 Color.clear.frame(height: 104.0)
             }
+            // Side Menu Start
+            .mask(RoundedRectangle(cornerRadius: 30.0, style: .continuous))
+            .rotation3DEffect(
+                .degrees(isOpen ? 30 : 0.0),axis: (x: 0.0, y: -1.0, z: 0.0)
+            )
+            .offset(x: isOpen ? 265.0 : 0.0)
+            .scaleEffect(isOpen ? 0.9 : 1.0)
+            // Side Menu End
+            .ignoresSafeArea()
             button.view()
                 .frame(width: 44.0, height: 44.0)
                 .mask(Circle())
                 .shadow(color: .shadow.opacity(0.2), radius: 5.0, x: 0.0, y: 5.0)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
                 .padding()
+                .offset(x: isOpen ? 216.0 : 0.0)
                 .onTapGesture {
                     button.setInput("isOpen", value: isOpen)
-                    isOpen.toggle()
+                    withAnimation(.spring(response: 0.5, dampingFraction: 0.7)) {
+                        isOpen.toggle()
+                    }
                 }
             TabBar()
+                .offset(y: isOpen ? 300.0 : 0.0)
         }
     }
 }
