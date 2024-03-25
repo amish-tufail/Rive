024fd6ffae6e585eaba1927a57e6cfbca021a0b4
Amish <amish.tufail2002@gmail.com>
2024-03-25 13:44:16 +0500

    - hamburger

--- Rive/Views/ContentView.swift
+++ Rive/Views/ContentView.swift
@@ -6,9 +6,12 @@
 //
 
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    @State private var isOpen = false
+    let button = RiveViewModel(fileName: "menu_button", stateMachineName: "State Machine", autoPlay: false)
     var body: some View {
         ZStack {
             switch selectedTab {
@@ -23,6 +26,16 @@ struct ContentView: View {
             case .user:
                 Text("User")
             }
+            button.view()
+                .frame(width: 44.0, height: 44.0)
+                .mask(Circle())
+                .shadow(color: .shadow.opacity(0.2), radius: 5.0, x: 0.0, y: 5.0)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                .padding()
+                .onTapGesture {
+                    try? button.setInput("isOpen", value: isOpen)
+                    isOpen.toggle()
+                }
             TabBar()
         }
     }
